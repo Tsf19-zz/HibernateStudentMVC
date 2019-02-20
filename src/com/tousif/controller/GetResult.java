90acaf514ffff9a96841e58b6091bcf4c8ea9c9c
Tousif <md.tousifmemon@gmail.com>
2019-02-20 14:06:10 +0530

    Second Commit

--- src/com/tousif/controller/GetResult.java
+++ src/com/tousif/controller/GetResult.java
@@ -2,6 +2,7 @@ package com.tousif.controller;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import com.tousif.model.Model;
 /**
  * Servlet implementation class GetResult
  */
+@WebServlet("/GetResultServlet")
 public class GetResult extends HttpServlet {
 
 	public void service(HttpServletRequest request, HttpServletResponse response) {
